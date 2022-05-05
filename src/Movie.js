4728d5c2106c703e9dbe316de646cab9ff9c9498
JeffAllison <itsjallison@gmail.com>
2022-05-05 10:00:38 -0700

    added movie.js function movie

--- src/Movie.js
+++ src/Movie.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
-export default function Movie() {
-  return <div>Movie</div>;
+export default function Movie({ posterColor, title, year, deleteMovieById }) {
+  return (
+    <div
+      onClick={() => deleteMovieById(title)}
+      className="movie-poster"
+      style={{ background: posterColor }}
+    >
+      <h3>{title}</h3>
+      <p>{year}</p>
+    </div>
+  );
 }
