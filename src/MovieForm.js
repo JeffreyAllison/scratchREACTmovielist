f66aea8638a29ba11f4d2824e38877d77755b6e7
JeffAllison <itsjallison@gmail.com>
2022-05-05 12:06:58 -0700

    imported props into movieform.js

--- src/MovieForm.js
+++ src/MovieForm.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
-export default function MovieForm() {
-  return <div>MovieForm</div>;
+export default function MovieForm({
+  movieFormTitle,
+  setMovieFormTitle,
+  movieFormYearReleased,
+  setMovieFormYearReleased,
+  movieFormDirector,
+  setMovieFormDirector,
+  movieFormColor,
+  setMovieFormColor,
+  submitMovie,
+}) {
+  return <div></div>;
 }
