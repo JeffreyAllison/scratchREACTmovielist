b7223d0c39e719b32966c98dffeae070f9a826f3
JeffAllison <itsjallison@gmail.com>
2022-05-05 12:34:09 -0700

    added JSX rendering to movieform.js

--- src/MovieForm.js
+++ src/MovieForm.js
@@ -11,5 +11,49 @@ export default function MovieForm({
   setMovieFormColor,
   submitMovie,
 }) {
-  return <div></div>;
+  return (
+    <div className="movie-form-container quarter">
+      <form onSubmit={submitMovie} className="movie-form">
+        <label>
+          Title
+          <input
+            required
+            value={movieFormTitle}
+            onChange={(e) => setMovieFormTitle(e.target.value)}
+          />
+        </label>
+        <label>
+          Year Released
+          <input
+            required
+            value={movieFormYearReleased}
+            onChange={(e) => setMovieFormYearReleased(e.target.value)}
+          />
+        </label>
+        <label>
+          Director
+          <input
+            required
+            value={movieFormDirector}
+            onChange={(e) => setMovieFormDirector(e.target.value)}
+          />
+        </label>
+        <label>
+          Poster Color
+          <select
+            required
+            value={movieFormColor}
+            onChange={(e) => setMovieFormColor(e.target.value)}
+          >
+            <option value="lightgreen">Green</option>
+            <option value="lightblue">Blue</option>
+            <option value="pink">Pink</option>
+            <option value="purple">Purple</option>
+            <option value="orange">Orange</option>
+          </select>
+        </label>
+        <button>Add Movie to List</button>
+      </form>
+    </div>
+  );
 }
