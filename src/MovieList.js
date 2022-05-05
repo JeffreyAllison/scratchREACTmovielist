ff494693396d3e8bcb834fcac719367a719e1e8b
JeffAllison <itsjallison@gmail.com>
2022-05-05 11:59:12 -0700

    completed movielist.js component

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import Movie from './Movie';
 
-export default function MovieList() {
-  return <div>MovieList</div>;
+export default function MovieList({ movies, handleDeleteMovie }) {
+  return (
+    <div className="movie-list">
+      {movies.map((movie, i) => (
+        <Movie
+          key={`${movie.movieFormYearReleased}-${movie.movieFormTitle}-${i}`}
+          movieFormTitle={movie.movieFormTitle}
+          movieFormYearReleased={movie.movieFormYearReleased}
+          movieFormDirector={movie.movieFormDirector}
+          movieFormColor={movie.movieFormColor}
+          handleDeleteMovie={handleDeleteMovie}
+        />
+      ))}
+    </div>
+  );
 }
